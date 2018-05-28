29bd88bac0fc308cd45cc828a55a0bc9ce2d5ce8
Rajat Chandra <rajatchandra4@gmail.com>
2018-05-28 23:40:48 +0530

    hw added

--- TwitterSentimentAnalysis.py
+++ TwitterSentimentAnalysis.py
@@ -1,5 +1,6 @@
 import tweepy
 from textblob import TextBlob
+import csv
 
 consumer_key = 'LZAACT1SMSCyBGKRQCDYOmQcP'
 consumer_secret = 'xkUBqaJUKs3GngpAEak4NBkKfPUxP4qpIvNchO4zUb5MVHFeqK'
@@ -12,9 +13,21 @@ auth.set_access_token(access_token, accesstoken_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search('obama')
+public_tweets = api.search('oneplus6')
 
+
+tweet_text = []
+analysis_result = []
 for tweet in public_tweets:
-	print(tweet.text)
+	#print(tweet.text)
+	tweet_text.append(tweet.text)
 	analysis = TextBlob(tweet.text)
-	print(analysis.sentiment)
+	if analysis.sentiment.polarity>0.3 and analysis.sentiment.subjectivity<0.5:
+		analysis_result.append('Fairly Positive')
+	else: analysis_result.append('Negative or Neutral')
+
+rows = zip(tweet_text,analysis_result)
+with open('twitterexport.csv', 'w',encoding='utf-8') as myfile:
+    writer = csv.writer(myfile)
+    for row in rows:
+        writer.writerow(row)
